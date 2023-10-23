65a9f84096d4252ca5abb97ee43ae3e0e08b446b
juliaooliveira <148073511+juliaooliveira@users.noreply.github.com>
2023-10-23 10:13:57 -0300

    .

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ function tocaSomPom () {
 
 const listaTeclas = document.querySelector('.tecla')
 let contador = 0,
-while(){ 
-    listaTeclas[contador].onclick = tocaSomPom;
+for (let contador = 0; contador < listaDeTeclas.length; contador++){ 
+    const tecla = listadeTeclas[Contador]
+    const instrumento = tecla.classlist[1];
+    const idaudio = `#som_ ${instrumento}`;
+        console.log(instrumento);
+    tecla.onclick = function (){
+        tocaSom(idaudio)
+    }
     contador = contador +1;
 
 }
